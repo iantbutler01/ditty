673f4760506618b7b6e1e58de553ccab41e80ce4
Ian Butler <itb206@nyu.edu>
2023-05-21 03:05:02 +0000

    Truncate and properly handle code examples

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import transformers
 from itertools import chain
 from transformers import AutoTokenizer, AutoModelForCausalLM, HfArgumentParser, TrainingArguments, BitsAndBytesConfig
 from datasets import load_dataset
-from peft import TaskType, LoraConfig, PeftConfig, PeftModel, get_peft_model, prepare_model_for_int8_training
+from peft import TaskType, LoraConfig, PeftConfig, PeftModel, get_peft_model
 from typing import Optional
 import torch
 from dataclasses import dataclass, field
-BitsAndBytesConfig(llm_int8_enable_fp32_cpu_offload=True)
+BitsAndBytesConfig()
 
 @dataclass
 class ModelArguments:
@@ -33,56 +33,67 @@ class DataTrainingArguments:
     )
     block_size: Optional[int] = field(
         default=1024, metadata={"help": "The maximum length of a training sample (in tokens)."}
-    )
+    ),
 
 parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
 
 model_args, data_args, training_args = parser.parse_args_into_dataclasses()
 
-tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path, use_fast=True)
+tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path)
 if tokenizer.pad_token_id is None:
     tokenizer.pad_token_id = tokenizer.eos_token_id
 
-model = AutoModelForCausalLM.from_pretrained(model_args.model_name_or_path, load_in_8bit=True, device_map="auto")
+model = AutoModelForCausalLM.from_pretrained(model_args.model_name_or_path, device_map="auto").cuda()
 
 target_modules = ["query_key_value", "xxx"]
 peft_config = LoraConfig(
     task_type=TaskType.CAUSAL_LM, target_modules=target_modules, inference_mode=False, r=16, lora_alpha=32, lora_dropout=0.05
 )
 
-model = prepare_model_for_int8_training(model)
+# Enable grads for checkpointing
+model.enable_input_require_grads()
+
 model = get_peft_model(model, peft_config)
 
-block_size = data_args.block_size
-
-def group_texts(examples):
-    # Concatenate all texts.
-    concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
-    total_length = len(concatenated_examples[list(examples.keys())[0]])
-    # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
-    # customize this part to your needs.
-    if total_length >= block_size:
-        total_length = (total_length // block_size) * block_size
-    # Split by chunks of max_len.
-    result = {
-        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
-        for k, t in concatenated_examples.items()
-    }
-    result["labels"] = result["input_ids"].copy()
-    return result
-
+block_size = 1024
 
 # ### Training
 data = load_dataset(data_args.dataset_name, data_args.dataset_language)
 columns = data["train"].features
-data = data.map(lambda samples: tokenizer(samples["whole_func_string"]), batched=True, remove_columns=columns)
-data = data.map(group_texts, batched=True)
+
+def filter_longer(sample):
+    tokens, attn_mask = tokenizer(sample["whole_func_string"])
+
+    return len(tokens) <= block_size
+
+data = data.filter(filter_longer)
+data = data.map(lambda sample: tokenizer(sample["whole_func_string"],
+                                         padding='max_length',
+                                         max_length=block_size), batched=True, remove_columns=columns)
+
+def truncate(sample):
+    sample["attention_mask"] = sample["attention_mask"][:block_size]
+    sample["input_ids"] = sample["input_ids"][:block_size]
+
+    return sample
+
+data = data.map(truncate)
 
 model.gradient_checkpointing_enable()
 trainer = transformers.Trainer(
     model=model,
     train_dataset=data["train"],
-    args=training_args,
+    args=transformers.TrainingArguments(
+        per_device_train_batch_size=4,
+        gradient_accumulation_steps=4,
+        warmup_steps=100,
+        # max_steps=4,
+        max_steps=200,
+        learning_rate=2e-4,
+        fp16=True,
+        logging_steps=1,
+        output_dir='outputs'
+    ),
     data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
 )
 model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
