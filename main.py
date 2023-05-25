e863679a96c49e24c72740f15c0c3d86e9d7bb61
Ian <itb206@nyu.edu>
2023-05-25 03:31:48 +0000

    Add proper checkpointing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import fire
-from lib.pipeline import Pipeline
+from lib.ditty.pipeline import Pipeline
 
 if __name__ == "__main__":
     fire.Fire(Pipeline)
---
+++ setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ditty',
+    version='0.1.3',
+    license='Apache V2',
+    author="Ian T Butler (KinglyCrow)",
+    author_email='iantbutler01@gmail.com',
+    packages=find_packages('lib'),
+    package_dir={'': 'lib'},
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/iantbutler01/ditty',
+    keywords='finetuning, llm, nlp, machine learning',
+    install_requires=[
+          'accelerate',
+          'transformers',
+          'datasets',
+          'bitsandbytes',
+          'fire',
+          'peft'
+      ],
+
+)
